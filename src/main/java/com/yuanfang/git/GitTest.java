cbe36279dd706aec4ff34a6665940eebd5253db0
ming1212 <104972349+ming1212@users.noreply.github.com>
2023-04-30 22:11:18 +0800

    pull commit

--- src/main/java/com/yuanfang/git/GitTest.java
+++ src/main/java/com/yuanfang/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hello master");
         System.out.println("hello hot-fix 2");
         System.out.println("push test");
+        System.out.println("pull test");
 
     }
 
