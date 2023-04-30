4bfc98c8aa71f97016901c35587c0112c7258a34
Yuanfang <Yuanfang@qq.com>
2023-04-30 22:04:06 +0800

    push commit

--- src/main/java/com/yuanfang/git/GitTest.java
+++ src/main/java/com/yuanfang/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello hot-fix");
         System.out.println("hello master");
         System.out.println("hello hot-fix 2");
+        System.out.println("push test");
 
     }
 
